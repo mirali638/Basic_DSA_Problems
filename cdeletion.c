890ffd5954667ee86e54293e45e10ba1da485c49
Mirali Jivani <146528110+mirali638@users.noreply.github.com>
2024-09-11 00:58:19 +0530

    Add files via upload

---
+++ cdeletion.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+void linkedListTraversal(struct Node *head)
+{
+    struct Node *ptr = head;
+    do
+    {
+        printf("Element is %d\n", ptr->data);
+        ptr = ptr->next;
+    } while (ptr != head);
+}
+
+struct Node *deleteAtFirst(struct Node *head)
+{
+    struct Node *ptr = head;
+    while (ptr->next != head)
+    {
+        ptr = ptr->next;
+    }
+    ptr->next = head->next;
+    struct Node *newHead = head->next;
+    free(head);
+
+    return newHead;
+}
+struct Node *deleteAtLast(struct Node *head)
+{
+    struct Node *p = head;
+    struct Node *q = head->next;
+    while (q->next != head)
+    {
+        p = p->next;
+        q = q->next;
+    }
+
+    p->next = head;
+    free(q);
+    return head;
+}
+
+struct Node *deleteAtIndex(struct Node *head, int index)
+{
+    struct Node *p = head;
+    struct Node *q = head->next;
+    for (int i = 0; i < index - 1; i++)
+    {
+        p = p->next;
+        q = q->next;
+    }
+
+    p->next = q->next;
+    free(q);
+    return head;
+}
+
+int main()
+{
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *fourth;
+
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *)malloc(sizeof(struct Node));
+
+    head->data = 4;
+    head->next = second;
+
+    second->data = 3;
+    second->next = third;
+
+    third->data = 6;
+    third->next = fourth;
+
+    fourth->data = 1;
+    fourth->next = head;
+
+    printf("Linked list before deletion\n");
+    linkedListTraversal(head);
+
+    head = deleteAtFirst(head);
+    
+    printf("Linked list after deletion\n");
+    linkedListTraversal(head);
+
+    return 0;
+}
